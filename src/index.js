f17b4bb32c1d58380bec9d34ddc887cccc4e5ab0
Will <willzhao77@gmail.com>
2020-12-09 21:44:41 +1300

    updated init

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import { render } from "react-dom";
 
-function App() {
-    const [state, setState] = useState("CLICK ME");
+// function App() {
+//     const [state, setState] = useState("CLICK ME");
 
-    return <button onClick={() => setState("CLICKED")}>{state}</button>;
-}
+//     return <button onClick={() => setState("CLICKED")}>{state}</button>;
+// }
 
-render(<App />, document.getElementById("root"));
+// render(<App />, document.getElementById("root"));
+
+const myh1 = <div>test</div>
+
+render(myh1, document.getElementById('root'))
